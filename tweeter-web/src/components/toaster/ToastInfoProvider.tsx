6def43955dc2b4f890e32289d52851dfa1b74711
Jack Johnson <jrj221@byu.edu>
2026-02-23 22:08:36 -0700

    M2C: Did configuration for testing

--- tweeter-web/src/components/toaster/ToastInfoProvider.tsx
+++ tweeter-web/src/components/toaster/ToastInfoProvider.tsx
@@ -4,69 +4,61 @@ import PropTypes from "prop-types";
 import { ToastListContext, ToastActionsContext } from "./ToastContexts";
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const ToastInfoProvider: React.FC<Props> = ({ children }) => {
-  const [toastList, setToastList] = useState<Toast[]>([]);
+	const [toastList, setToastList] = useState<Toast[]>([]);
 
-  const displayExistingToast = useCallback((toast: Toast) => {
-    setToastList((previousList) => [...previousList, toast]);
-  }, []);
+	const displayExistingToast = useCallback((toast: Toast) => {
+		setToastList((previousList) => [...previousList, toast]);
+	}, []);
 
-  const displayToast = useCallback(
-    (
-      toastType: ToastType,
-      message: string,
-      duration: number,
-      title?: string,
-      bootstrapClasses?: string
-    ): string => {
-      const toast = makeToast(
-        toastType,
-        message,
-        duration,
-        title,
-        bootstrapClasses
-      );
-      displayExistingToast(toast);
-      return toast.id;
-    },
-    [displayExistingToast]
-  );
+	const displayToast = useCallback(
+		(
+			toastType: ToastType,
+			message: string,
+			duration: number,
+			title?: string,
+			bootstrapClasses?: string
+		): string => {
+			const toast = makeToast(toastType, message, duration, title, bootstrapClasses);
+			displayExistingToast(toast);
+			return toast.id;
+		},
+		[displayExistingToast]
+	);
 
-  const deleteToast = useCallback((id: string) => {
-    setToastList((currentList) => {
-      const filtered = currentList.filter((x) => x.id !== id);
-      return filtered;
-    });
-  }, []);
+	const deleteToast = useCallback((id: string) => {
+		setToastList((currentList) => {
+			const filtered = currentList.filter((x) => x.id !== id);
+			return filtered;
+		});
+	}, []);
 
-  const deleteAllToasts = useCallback(() => {
-    setToastList([]);
-  }, []);
+	const deleteAllToasts = useCallback(() => {
+		setToastList([]);
+	}, []);
 
-  const toastActions = useMemo(
-    () => ({
-      displayExistingToast,
-      displayToast,
-      deleteToast,
-      deleteAllToasts,
-    }),
-    [displayExistingToast, displayToast, deleteToast, deleteAllToasts]
-  );
+	const toastActions = useMemo(
+		() => ({
+			displayExistingToast,
+			displayToast,
+			deleteToast,
+			deleteAllToasts,
+		}),
+		[displayExistingToast, displayToast, deleteToast, deleteAllToasts]
+	);
 
-  return (
-    <ToastListContext.Provider value={toastList}>
-      <ToastActionsContext.Provider value={toastActions}>
-        {children}
-      </ToastActionsContext.Provider>
-    </ToastListContext.Provider>
-  );
+	return (
+		<ToastListContext.Provider value={toastList}>
+			<ToastActionsContext.Provider value={toastActions}>{children}</ToastActionsContext.Provider>
+		</ToastListContext.Provider>
+	);
 };
 
 ToastInfoProvider.propTypes = {
-  children: PropTypes.element.isRequired,
+	children: PropTypes.element.isRequired,
 };
 
 export default ToastInfoProvider;
