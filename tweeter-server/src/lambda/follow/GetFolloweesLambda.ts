c33c3cc54a43f4e54eb1510095dd08fe06107c98
Jack Johnson <jrj221@byu.edu>
2026-03-13 17:53:32 -0600

    M3: Made (in code) lambda functions for loadMoreFollowers and loadMoreFollowees, which required making a UserDTO and PagedUserItem Request and Response interfaces. Re-committing since I forgot to add files from other modules

---
+++ tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
@@ -0,0 +1,9 @@
+import { PagedUserItemRequest, PagedUserItemResponse } from "tweeter-shared";
+import { FollowService } from "../../model/service/FollowService";
+
+// This function gets called by API Gateway when you make a request
+export const handler = async (request: PagedUserItemRequest): Promise<PagedUserItemResponse> => {
+    const followService = new FollowService();
+    const [items, hasMore] = await followService.loadMoreFollowees(request.token, request.userAlias, request.pageSize, request.lastItem);
+    return {success: true, message: null, items: items, hasMoreItems: hasMore};
+} 
---
+++ tweeter-server/src/model/service/FollowService.ts
@@ -0,0 +1,75 @@
+import { AuthToken, User, FakeData, UserDTO } from "tweeter-shared";
+import { Service } from "./Service";
+
+export class FollowService implements Service {
+	public async loadMoreFollowees(
+		token: string,
+		userAlias: string,
+		pageSize: number,
+		lastItem: UserDTO | null
+	): Promise<[UserDTO[], boolean]> {
+		// TODO: Replace with the result of calling server
+		return this.getFakeData(lastItem, pageSize, userAlias); 
+    }
+
+	public async loadMoreFollowers(
+		token: string,
+		userAlias: string,
+		pageSize: number,
+		lastItem: UserDTO | null
+	): Promise<[UserDTO[], boolean]> {
+		// TODO: Replace with the result of calling server
+		return this.getFakeData(lastItem, pageSize, userAlias); 
+    }
+
+    private async getFakeData(lastItem: UserDTO | null, pageSize: number, userAlias: string): Promise<[UserDTO[], boolean]> {
+        const [items, hasMoreItems] = FakeData.instance.getPageOfUsers(User.fromDTO(lastItem), pageSize, userAlias);
+        const dtos = items.map((user) => user.DTO);
+        return [dtos, hasMoreItems];
+    }
+
+
+	public async getIsFollowerStatus(authToken: AuthToken, user: User, selectedUser: User): Promise<boolean> {
+		// TODO: Replace with the result of calling server
+		return FakeData.instance.isFollower();
+	}
+
+	public async getFolloweeCount(authToken: AuthToken, user: User): Promise<number> {
+		// TODO: Replace with the result of calling server
+		return FakeData.instance.getFolloweeCount(user.alias);
+	}
+
+	public async getFollowerCount(authToken: AuthToken, user: User): Promise<number> {
+		// TODO: Replace with the result of calling server
+		return FakeData.instance.getFollowerCount(user.alias);
+	}
+
+	public async changeFollowingStatus(
+		authToken: AuthToken,
+		userToChangeFollowingStatusFor: User
+	): Promise<[followerCount: number, followeeCount: number]> {
+		// Pause so we can see the follow message. Remove when connected to the server
+		await new Promise((f) => setTimeout(f, 2000));
+
+		// TODO: Call the server
+
+		const followerCount = await this.getFollowerCount(authToken, userToChangeFollowingStatusFor);
+		const followeeCount = await this.getFolloweeCount(authToken, userToChangeFollowingStatusFor);
+
+		return [followerCount, followeeCount];
+	}
+
+	public follow = async (
+		authToken: AuthToken,
+		userToFollow: User
+	): Promise<[followerCount: number, followeeCount: number]> => {
+		return await this.changeFollowingStatus(authToken, userToFollow);
+	};
+
+	public unfollow = async (
+		authToken: AuthToken,
+		userToUnfollow: User
+	): Promise<[followerCount: number, followeeCount: number]> => {
+		return await this.changeFollowingStatus(authToken, userToUnfollow);
+	};
+}
