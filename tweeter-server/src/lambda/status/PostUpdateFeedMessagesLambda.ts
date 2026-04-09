f2d80d76376b8968e14d8e1501d032598c6a2d7e
Jack Johnson <jrj221@byu.edu>
2026-04-08 18:47:05 -0600

    M4B: Changed new lambdas to recieve a SQSEvent object and extract the message from it

--- tweeter-server/src/lambda/status/PostUpdateFeedMessagesLambda.ts
+++ tweeter-server/src/lambda/status/PostUpdateFeedMessagesLambda.ts
@@ -1,21 +1,25 @@
+import { SQSEvent } from "aws-lambda";
 import { PostStatusMessage, UpdateFeedMessage, UserDTO } from "tweeter-shared";
 import { ServerFollowService } from "../../model/service/ServerFollowService";
 import { DynamoDBDAOFactory } from "../../dao/DynamoDBDAO/DynamoDBFactory";
 import { sendSQSMessage } from "./MessageQueue";
 
-export const handler = async (message: string) => {
-	const postStatusMessage: PostStatusMessage = JSON.parse(message); // More typechecking?
+export const handler = async (event: SQSEvent) => {
+	for (const record of event.Records) {
+		// Based on configured batch size
+		const postStatusMessage: PostStatusMessage = JSON.parse(record.body); // More typechecking?
 
-	const followers: string[] = await getAllFollowers(postStatusMessage.followeeAlias, postStatusMessage.token);
+		const followers: string[] = await getAllFollowers(postStatusMessage.followeeAlias, postStatusMessage.token);
 
-	for (let i = 0; i < followers.length; i += 400) {
-		const followersSubset = followers.slice(i, i + 400);
-		const message: UpdateFeedMessage = {
-			followees: followersSubset,
-			statusDTO: postStatusMessage.statusDTO,
-			token: postStatusMessage.token,
-		};
-		await sendSQSMessage("https://sqs.us-east-1.amazonaws.com/735980888276/UpdateFeed", message);
+		for (let i = 0; i < followers.length; i += 400) {
+			const followersSubset = followers.slice(i, i + 400);
+			const message: UpdateFeedMessage = {
+				followees: followersSubset,
+				statusDTO: postStatusMessage.statusDTO,
+				token: postStatusMessage.token,
+			};
+			await sendSQSMessage("https://sqs.us-east-1.amazonaws.com/735980888276/UpdateFeed", message);
+		}
 	}
 };
 
--- tweeter-server/src/lambda/status/UpdateFeedsLambda.ts
+++ tweeter-server/src/lambda/status/UpdateFeedsLambda.ts
@@ -1,22 +1,30 @@
+import { SQSEvent } from "aws-lambda";
 import { UpdateFeedMessage } from "tweeter-shared";
 import { ServerStatusService } from "../../model/service/ServerStatusService";
 import { DynamoDBDAOFactory } from "../../dao/DynamoDBDAO/DynamoDBFactory";
 
-export const handler = async (message: string) => {
-	const updateFeedMessage: UpdateFeedMessage = JSON.parse(message); // More typechecking?
+export const handler = async (event: SQSEvent) => {
+	for (const record of event.Records) {
+		// Based on configured batch size
+		const updateFeedMessage: UpdateFeedMessage = JSON.parse(record.body); // More typechecking?
 
-	const statusService = new ServerStatusService(new DynamoDBDAOFactory());
-	for (let i = 0; i < updateFeedMessage.followees.length; i += 100) {
-		const startTimeMilllis = Date.now();
+		const statusService = new ServerStatusService(new DynamoDBDAOFactory());
+		for (let i = 0; i < updateFeedMessage.followees.length; i += 100) {
+			const startTimeMilllis = Date.now();
 
-		for (let j = 0; j < 4; j++) {
-			const followerBatch = updateFeedMessage.followees.slice(i + j * 25, i + (j + 1) * 25);
-			await statusService.batchAddFeedItems(updateFeedMessage.token, updateFeedMessage.statusDTO, followerBatch);
-		}
+			for (let j = 0; j < 4; j++) {
+				const followerBatch = updateFeedMessage.followees.slice(i + j * 25, i + (j + 1) * 25);
+				await statusService.batchAddFeedItems(
+					updateFeedMessage.token,
+					updateFeedMessage.statusDTO,
+					followerBatch,
+				);
+			}
 
-		const elapsedTimeMillis = Date.now() - startTimeMilllis;
-		if (elapsedTimeMillis > 1000) {
-			await new Promise((resolve) => setTimeout(resolve, 1000 - elapsedTimeMillis));
+			const elapsedTimeMillis = Date.now() - startTimeMilllis;
+			if (elapsedTimeMillis > 1000) {
+				await new Promise((resolve) => setTimeout(resolve, 1000 - elapsedTimeMillis));
+			}
 		}
 	}
 };
